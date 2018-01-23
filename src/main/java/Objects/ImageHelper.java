fcb766a2511b22dc44fc177153627e1913b62cab
Sumpf <stefan.kuhn@hotmail.com>
2018-01-23 23:32:35 +0100

    fixed images, created image folder in ressources

--- src/main/java/Objects/ImageHelper.java
+++ src/main/java/Objects/ImageHelper.java
@@ -9,20 +9,21 @@ public class ImageHelper {
 
     Image bKing, bKnight, bPawn, bQueen, bRook, bRunner, wKing, wKnight, wPawn, wQueen, wRook, wRunner;
 
-    private ImageHelper() throws FileNotFoundException {
-        this.bKing = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackking.bmp")));
-        this.bKnight = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackknight.bmp")));
-        this.bPawn = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackpawn.bmp")));
-        this.bQueen = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackqueen.bmp")));
-        this.bRook = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackrook.bmp")));
-        this.bRunner = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackrunner.bmp")));
-        this.wKing = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiteking.bmp")));
-        this.wKnight = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiteknight.bmp")));
-        this.wPawn = new Image(new FileInputStream(new File("src\\main\\java\\images\\whitepawn.bmp")));
-        this.wQueen = new Image(new FileInputStream(new File("src\\main\\java\\images\\whitequeen.bmp")));
-        this.wRook = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiterook.bmp")));
-        this.wRunner = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiterunner.bmp")));
-    }
+ private ImageHelper() throws FileNotFoundException{
+  this.bKing  = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackking.png")));
+  this.bKnight  = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackknight.png")));
+  this.bPawn = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackpawn.png")));
+  this.bQueen = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackqueen.png")));
+  this.bRook  = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackrook.png")));
+  this.bRunner = new Image(new FileInputStream(new File("src\\main\\java\\images\\blackrunner.png")));
+  this.wKing  = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiteking.png")));
+  this.wKnight  = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiteknight.png")));
+  this.wPawn = new Image(new FileInputStream(new File("src\\main\\java\\images\\whitepawn.png")));
+  this.wQueen = new Image(new FileInputStream(new File("src\\main\\java\\images\\whitequeen.png")));
+  this.wRook  = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiterook.png")));
+  this.wRunner = new Image(new FileInputStream(new File("src\\main\\java\\images\\whiterunner.png")));
+ }
+
 
     private static ImageHelper instance;
 
